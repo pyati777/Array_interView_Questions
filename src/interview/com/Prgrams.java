5645cdc17e99e7d905ae158ea11d54976b20b8bb
karthik <pyatikarthik11@gmail.com>
2023-08-16 15:46:29 +0530

    Created Message Prgrms.java

---
+++ src/interview/com/Prgrams.java
@@ -0,0 +1,11 @@
+package interview.com;
+
+public class Prgrams {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Array Programs");
+	}
+
+}
